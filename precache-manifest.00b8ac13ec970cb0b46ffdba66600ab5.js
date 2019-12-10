8a2a7a4acc5ba2b1a5c0cccd5415ccdbd1bbacc8
Shihao Wen <wenshihao22@gmail.com>
2019-12-10 15:56:25 -0500

    Updates

--- precache-manifest.00b8ac13ec970cb0b46ffdba66600ab5.js
+++ precache-manifest.00b8ac13ec970cb0b46ffdba66600ab5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "875ea8b46cb359ec906fec2d27e5e5cd",
+    "revision": "706d02e42b76340c317fe39b8263ac1f",
     "url": "/NBA/index.html"
   },
   {
@@ -8,15 +8,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/NBA/static/css/2.aa7fcd32.chunk.css"
   },
   {
-    "revision": "652be091feb263438ce0",
-    "url": "/NBA/static/css/main.12e69ca9.chunk.css"
+    "revision": "2fc145e070ad6e579316",
+    "url": "/NBA/static/css/main.a7830efa.chunk.css"
   },
   {
     "revision": "8adff0f602fb81afb542",
     "url": "/NBA/static/js/2.6e6f4f4b.chunk.js"
   },
   {
-    "revision": "652be091feb263438ce0",
+    "revision": "2fc145e070ad6e579316",
     "url": "/NBA/static/js/main.51a8b511.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/NBA/precache-manifest.740583c42a2473af07b36a5067d071f1.js"
+  "/NBA/precache-manifest.00b8ac13ec970cb0b46ffdba66600ab5.js"
 );
 
 self.addEventListener('message', (event) => {
